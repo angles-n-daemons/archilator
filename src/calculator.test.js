2ffe2ca663b9ccb52328ba96dbbf32eda647bc5a
Brian Dillmann <dillmann.brian@gmail.com>
2023-08-08 13:57:14 -0400

    placeholder test for the calculator test file

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,8 +1,6 @@
 const Calculator = require('./calculator.js');
 
 describe('Calculator', function() {
-    it('is a test so jest doesnt complain', function() {
-
-    });
+    it('is a test so jest doesnt complain', function() {});
     // tests to be added when the project is complete
 });
